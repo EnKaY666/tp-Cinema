c5f410d24020f6b369518864f3d3e2d5f7147c82
Adam Fouquet <sio@S-204-17>
2019-09-16 09:29:37 +0200

    Premiere initialisation du projet

---
+++ cine/salleCinema.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cine
+{
+    class SalleCinema
+    {
+        private string film;
+        private int nbPlace;
+        private double prixNormal;
+
+    }
+}
